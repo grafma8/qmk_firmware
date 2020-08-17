65d3d140a6cd98bc93fd44ea8f7ff2d7d242dae1
grafma8 <gra.fmatsu8@gmail.com>
2020-08-17 11:01:37 +0900

    update and make my keymap

--- keyboards/ergodash/rev1/keymaps/grafma-v1/keymap.c
+++ keyboards/ergodash/rev1/keymaps/grafma-v1/keymap.c
@@ -21,9 +21,9 @@ enum custom_keycodes {
 #define CTLESC MT(MOD_LCTL, KC_ESC)
 #define SFTSPC MT(MOD_LSFT, KC_SPC)
 #define ALTRGHT LALT(KC_RGHT)
-#define ALTLFT LALT(KC_LFT)
+#define ALTLFT LALT(KC_LEFT)
 #define VW_RGHT LCTL(LGUI(KC_RGHT))
-#define VW_LFT LCTL(LGUI(KC_LFT))
+#define VW_LFT LCTL(LGUI(KC_LEFT))
 
 /* jtu keycodes
   JU_2
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JU_MINS,                        JU_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JU_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JU_GRV ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    JU_SCLN, JU_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOWER,   SFTSPC , SFTSPC,       SP1ENT , KC_BSPC ,RAISE,            KC_LGUI, KC_LFT,  KC_UP,   KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOWER,   SFTSPC , SFTSPC,       SP1ENT , KC_BSPC ,RAISE,            KC_LGUI, KC_LEFT, KC_UP,   KC_RGHT  \
   ),
 
 
@@ -84,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   JU_LCBR,                        JU_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
+    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   JU_LBRC,                        JU_RBRC, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_EXLM, KC_UP,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_7 ,   KC_8,    KC_9,    KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_LEFT, KC_DOWN, KC_RGHT, KC_4,    KC_5,    KC_DEL ,                        KC_DEL,  KC_H,    KC_4   , KC_5   , KC_6   , KC_COLN, KC_DQT , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_0,    KC_1,    KC_2,    KC_3,    KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOWER,   KC_SPC ,KC_DEL,         KC_DEL, KC_ENT , RAISE,            KC_HOME, KC_LFT,  KC_DOWN, KC_RGHT   \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOWER,   KC_SPC ,KC_DEL,         KC_DEL, KC_ENT , RAISE,            KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT   \
   ),
 
   /* Raise
@@ -126,9 +126,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT(
     _______, _______, _______, _______, _______, _______,KC_MPRV,                       KC_MNXT, KC_MPLY, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR, \
     _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                       _______, VW_RGHT, VW_LFT, _______, _______, _______, _______, \
+    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                       _______, VW_RGHT, VW_LFT , _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______, _______  _______, _______, _______, _______  \
+    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______  \
   ),
 
 
@@ -153,7 +153,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 static bool     lower_pressed = false;
 static bool     raise_pressed = false;
-static bool     sp1ent_pressed = false
+static bool     sp1ent_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // jtu
